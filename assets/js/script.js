0a588e14aa872658bf4337163567c732397d3655
Gcz14 <guilhermeribeiroa2004@gmail.com>
2025-05-30 16:32:45 -0300

    Deploying project for presentation

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const elementos = document.querySelectorAll('[data-key]');
+let dadosTexto = null;
+
+function inserirTextos(data) {
+  elementos.forEach(el => {
+    const chave = el.dataset.key;
+    const valor = chave.split('.').reduce((obj, prop) => obj?.[prop], data) ?? '';
+    const contemHTML = typeof valor === 'string' && /<\/?[a-z][\s\S]*>/i.test(valor);
+    el[contemHTML ? 'innerHTML' : 'textContent'] = valor;
+    el.style.display = el.tagName === 'BUTTON' ? 'inline-block' : 'block';
+  });
+}
+
+fetch('/assets/textos.json')
+  .then(res => res.json())
+  .then(data => {
+    dadosTexto = data;
+    inserirTextos(data);
+  });
